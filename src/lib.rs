30695700a367fcfd40e5aaf0332e68506c41057a
Joshua Harris <joshuaweharris@googlemail.com>
2019-07-08 18:39:33 +0100

    Basic outline

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+fn build(source_dir: &str, target_dir: &str) {}
+
+fn build_posts(source_dir: &str, target_dir: &str) {}
+fn build_pages(source_dir: &str, target_dir: &str) {}
+
+fn convert_post(markdown: &str) -> String {}
+fn convert_page(template: &str) -> String {}
